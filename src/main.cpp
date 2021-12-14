478bb9896f4a8c6f1def143431dd864f52be801f
Dávid Ferenc Szabó <szabodavidferenc@gmail.com>
2021-12-15 00:04:37 +0100

    save: not fly away finally!

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,18 @@ int main(int argc, char **argv) {
 
   ros::NodeHandle n;
 
-  ros::Publisher left_back_leg_2 = n.advertise<std_msgs::Float64>(
-      "left_back_leg_2_controller/command", 1000);
-  ros::Publisher left_mid_leg_2 =
-      n.advertise<std_msgs::Float64>("left_mid_leg_2_controller/command", 1000);
-  ros::Publisher left_front_leg_2 = n.advertise<std_msgs::Float64>(
-      "left_front_leg_2_controller/command", 1000);
-  ros::Publisher right_back_leg_2 = n.advertise<std_msgs::Float64>(
-      "right_back_leg_2_controller/command", 1000);
-  ros::Publisher right_mid_leg_2 = n.advertise<std_msgs::Float64>(
-      "right_mid_leg_2_controller/command", 1000);
-  ros::Publisher right_front_leg_2 = n.advertise<std_msgs::Float64>(
-      "right_front_leg_2_controller/command", 1000);
+  ros::Publisher left_back_leg_3 = n.advertise<std_msgs::Float64>(
+      "left_back_leg_3_controller/command", 1000);
+  ros::Publisher left_mid_leg_3 =
+      n.advertise<std_msgs::Float64>("left_mid_leg_3_controller/command", 1000);
+  ros::Publisher left_front_leg_3 = n.advertise<std_msgs::Float64>(
+      "left_front_leg_3_controller/command", 1000);
+  ros::Publisher right_back_leg_3 = n.advertise<std_msgs::Float64>(
+      "right_back_leg_3_controller/command", 1000);
+  ros::Publisher right_mid_leg_3 = n.advertise<std_msgs::Float64>(
+      "right_mid_leg_3_controller/command", 1000);
+  ros::Publisher right_front_leg_3 = n.advertise<std_msgs::Float64>(
+      "right_front_leg_3_controller/command", 1000);
 
   std_msgs::Float64 position_msg;
   position_msg.data = -2 * PI / 3;
@@ -30,12 +30,12 @@ int main(int argc, char **argv) {
 
   while (ros::ok()) {
 
-      left_back_leg_2.publish(position_msg);
-  left_mid_leg_2.publish(position_msg);
-  left_front_leg_2.publish(position_msg);
-  //right_back_leg_2.publish(position_msg);
-  //right_mid_leg_2.publish(position_msg);
-  //right_front_leg_2.publish(position_msg);
+      left_back_leg_3.publish(position_msg);
+  left_mid_leg_3.publish(position_msg);
+  left_front_leg_3.publish(position_msg);
+  //right_back_leg_3.publish(position_msg);
+  //right_mid_leg_3.publish(position_msg);
+  //right_front_leg_3.publish(position_msg);
   }
 
   return 0;
